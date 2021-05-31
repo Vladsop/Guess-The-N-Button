697edc8e3d750546673204ff4d0081d1098aab2c
Vlad Soporan <73958706+Vladsop@users.noreply.github.com>
2021-05-31 22:29:30 +0300

    Updated printing messages function based on returned values.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 $("#restartgame").hide();
-var buttons_number = 0;
-var winner = 0;
-var button_name = "";
-var text = "";
-
+var buttons_number = null;
+var winner = null;
 //Mapping "Enter" key to trigger the "Generate Button"
 document.getElementById("buttons_number_input")
     .addEventListener("keyup", function(event) {
@@ -20,18 +17,17 @@ function generate_buttons() {
     buttons_number = document.getElementById("buttons_number_input").value;
     winner = Math.floor(Math.random() * parseInt(buttons_number)) + 1;
     if (buttons_number <= 0) {
-        text = "Please enter a number higher than 0!"
+        return 0;
     } else if (isNaN(buttons_number)) {
-        text = "Please enter a number and try again!"
+        return -1;
     } else {
-        document.getElementById("generate_buttons").onclick = null;
+        $("#generated_buttons").empty();
         for (let i = 1, j = 0; i <= buttons_number; ++i) {
-            button_name = "Button " + i.toString();
-            $('#generated_buttons').append('<button class="btn btn-dark" id=' + i + ' style="margin-right: 5px; margin-bottom: 5px;" onclick = "check_button(id);" > ' + button_name + ' < /button>');
+            let button_name = "Button " + i.toString();
+            $('#generated_buttons').append('<button class="btn btn-dark" id=' + i + ' style="margin-right: 5px; margin-bottom: 5px;" onclick="print_message(check_button(id));">' + button_name + '</button>');
         }
-        text = "Try your luck and pick one button. Will it be the winner? Let's see:"
+        return "pick";
     }
-    print_message();
 }
 
 //Checking if the winning button randomly generated in the function above is equal to the id of the clicked button.
@@ -45,12 +41,10 @@ function check_button(id) {
             }
         });
         $("#restartgame").show()
-        text = "Congratulations, You guessed it!"
-    } else {
-        $("#restartgame").show()
-        text = "Sorry, Would you like to try again?"
+        return true;
     }
-    print_message(text);
+    $("#restartgame").show()
+    return false;
 }
 
 //I think it's a good practice to restart the game on the same static webpage than doing a page refresh, the expected result is the same,
@@ -58,16 +52,28 @@ function check_button(id) {
 //Given the fact I'm disabling the "Generate buttons" button, i'm adding the click action after resetting the game.
 function reset() {
     document.getElementById("generate_buttons").onclick = function() {
-        generate_buttons()
+        print_message(generate_buttons());
     };
     buttons_number = document.getElementById("buttons_number_input").value;
     winner = Math.floor(Math.random() * parseInt(buttons_number)) + 1;
     $("#restartgame").hide();
     $("#generated_buttons").empty();
-    print_message(text = "");
+    return "reset";
 }
 
-//Printing useful information for users;
-function print_message(text) {
-    return document.getElementById("message").innerHTML = text;
+function print_message(result) {
+    if (result === "pick") {
+        message = "Try your luck and pick one button. Will it be the winner? Let's see:";
+    } else if (result === 0) {
+        message = "Please enter a number higher than 0!";
+    } else if (result === -1) {
+        message = "Please enter a number and try again!";
+    } else if (result === false) {
+        message = "Sorry, Would you like to try again?";
+    } else if (result === true) {
+        message = "Congratulations, You guessed it!";
+    } else if (result === "reset") {
+        message = " ";
+    }
+    return document.getElementById("message").innerHTML = message;
 }
