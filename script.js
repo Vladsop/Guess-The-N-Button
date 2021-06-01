8b47b8a74646cb9ff2132fa7c626d257cb820f1c
Vlad Soporan <73958706+Vladsop@users.noreply.github.com>
2021-06-01 17:57:23 +0300

    Updated printMessage function, updated variables names type to camelCase

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
 $("#restartgame").hide();
-var buttons_number = null;
+var buttonsNumber = null;
 var winner = null;
-//Mapping "Enter" key to trigger the "Generate Button"
-document.getElementById("buttons_number_input")
-    .addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.getElementById("generate_buttons").click();
-        }
-    });
 
 //Generating buttons according to the users input number, and based on that number will randomly generate a winning button.
 //Will alert the user to try again and input a number if the first input was text or another number if the first input is bellow or equal to 0.
-//Disabled the "Generate Button" after a valid input and made it to be available only after the restart of the app.
-function generate_buttons() {
-    buttons_number = document.getElementById("buttons_number_input").value;
-    winner = Math.floor(Math.random() * parseInt(buttons_number)) + 1;
-    if (buttons_number <= 0) {
-        return 0;
-    } else if (isNaN(buttons_number)) {
-        return -1;
+
+function generateButtons() {
+    buttonsNumber = document.getElementById("buttonsNumberInput").value;
+    winner = Math.floor(Math.random() * parseInt(buttonsNumber)) + 1;
+    if (buttonsNumber <= 0) {
+        return "Please enter a number higher than 0!";
+    } else if (isNaN(buttonsNumber)) {
+        return "Please enter a number and try again!";
     } else {
-        $("#generated_buttons").empty();
-        for (let i = 1, j = 0; i <= buttons_number; ++i) {
+        $("#generatedButtons").empty();
+        for (let i = 1, j = 0; i <= buttonsNumber; ++i) {
             let button_name = "Button " + i.toString();
-            $('#generated_buttons').append('<button class="btn btn-dark" id=' + i + ' style="margin-right: 5px; margin-bottom: 5px;" onclick="print_message(check_button(id));">' + button_name + '</button>');
+            $('#generatedButtons').append('<button class="btn btn-dark" id=' + i + ' style="margin-right: 5px; margin-bottom: 5px;" onclick="printMessage(checkButton(id));">' + button_name + '</button>');
         }
-        return "pick";
+        return "Try your luck and pick one button. Will it be the winner? Let's see:";
     }
 }
 
 //Checking if the winning button randomly generated in the function above is equal to the id of the clicked button.
-function check_button(id) {
+function checkButton(id) {
     if (Number(winner) === Number(id)) {
         confetti({
             particleCount: 500,
@@ -41,39 +33,26 @@ function check_button(id) {
             }
         });
         $("#restartgame").show()
-        return true;
+        return "Congratulations, You guessed it!";
     }
     $("#restartgame").show()
-    return false;
+    return "Sorry, Would you like to try again?";
 }
 
 //I think it's a good practice to restart the game on the same static webpage than doing a page refresh, the expected result is the same,
 //but the first option is faster.
-//Given the fact I'm disabling the "Generate buttons" button, i'm adding the click action after resetting the game.
 function reset() {
-    document.getElementById("generate_buttons").onclick = function() {
-        print_message(generate_buttons());
+    document.getElementById("generateButtons").onclick = function() {
+        printMessage(generateButtons());
     };
-    buttons_number = document.getElementById("buttons_number_input").value;
-    winner = Math.floor(Math.random() * parseInt(buttons_number)) + 1;
+    buttonsNumber = document.getElementById("buttonsNumberInput").value;
+    winner = Math.floor(Math.random() * parseInt(buttonsNumber)) + 1;
     $("#restartgame").hide();
-    $("#generated_buttons").empty();
-    return "reset";
+    $("#generatedButtons").empty();
+    return " ";
 }
 
-function print_message(result) {
-    if (result === "pick") {
-        message = "Try your luck and pick one button. Will it be the winner? Let's see:";
-    } else if (result === 0) {
-        message = "Please enter a number higher than 0!";
-    } else if (result === -1) {
-        message = "Please enter a number and try again!";
-    } else if (result === false) {
-        message = "Sorry, Would you like to try again?";
-    } else if (result === true) {
-        message = "Congratulations, You guessed it!";
-    } else if (result === "reset") {
-        message = " ";
-    }
+//Prints all messages according to parameters it receives from the other functions.
+function printMessage(message) {
     return document.getElementById("message").innerHTML = message;
 }
